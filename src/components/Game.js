24c59ad280b563e5e2013e5a229254c2364f1a57
Aaron Whitehead <whitehead.aaron@live.com>
2018-10-12 14:47:51 -0700

    finished skeleton for game page

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,8 +2,9 @@ import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
-
 export function Game(props) {
+  let deck = "deck";
+  const card = require(`../images/${deck}.jpg`);
   return (
     <div className="gameBack">
       <div className="gameGrid">
@@ -29,8 +30,36 @@ export function Game(props) {
             </button>
           </Link>
         </div>
+        <div className="dealerCard1">
+          <img src={card} alt="Dealer's first card" />
+        </div>
+        <div className="dealerCard2">
+          <img src={card} alt="Dealer's second card" />
+        </div>
         <div className="deckImgDiv">
-              <img src={require('../images/deck.jpg')} alt="Deck of Cards" />
+          <img src={card} alt="Deck of Cards" />
+        </div>
+        <button
+          className="hitButton"
+          onClick={() => {
+            console.log("hit");
+          }}
+        >
+          Hit
+        </button>
+        <button
+          className="stayButton"
+          onClick={() => {
+            console.log("stay");
+          }}
+        >
+          Stay
+        </button>
+        <div className="playerCard1">
+          <img src={card} alt="Player's first card" />
+        </div>
+        <div className="playerCard2">
+          <img src={card} alt="Player's second card" />
         </div>
       </div>
     </div>
